cc2218e737487609120c3a81b9ccf27bc8a663b0
songrit0 <songrit208133@gmail.com>
2022-11-05 18:17:15 +0700

    v2.01

---
+++ NuxtV10/pages/pages01.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <h1>{{demo}}</h1>
+   <h1 v-for="(data , index ) in news " :key="index">{{data}} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            demo: 'Hello',
+             news: [1,2,3,4,5],
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
