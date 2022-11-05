a4bad32f7f56e8d992bbae9e033fab52bfda840f
MingRmu126 <87534173+MingRmu126@users.noreply.github.com>
2022-11-05 18:37:54 +0700

    001

---
+++ NuxtV10/pages/pages01.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{data}}</h1>
+  </div>
+</template>
+
+<script>
+import {axios} from 'axios';
+export default {
+
+    data() {
+        return {
+            data : "Hi"
+        }
+    },
+    mounted() {
+        axios
+    },
+}
+</script>
+
+<style>
+
+</style>
